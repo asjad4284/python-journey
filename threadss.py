b58bfd3be1b717c168a553138d0c5f459994b015
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-01-09 21:28:10 +0500

    Implement basic threading example

--- threadss.py
+++ threadss.py
@@ -1,2 +1,24 @@
 import threading 
 import time
+
+def take_order():
+    for i in range(1,4):
+        print(f"Taking order #{i}")
+        time.sleep(2)
+
+def brew_chai():
+    for i in range(1,4):
+        print(f"Making chai for #{i}")
+        time.sleep(3)
+
+order_thread=threading.Thread(target=take_order)
+brew_thread=threading.Thread(target=brew_chai)
+
+order_thread.start()
+brew_thread.start()
+
+# wait for both to finish
+order_thread.join()
+brew_thread.join()
+
+print("All the orders have been served!")
