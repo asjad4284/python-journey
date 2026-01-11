1592d6403d0517f68cb895bd60ee594d4bc48fb1
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-01-11 13:19:57 +0500

    Understanding runtime error

---
+++ global_interpreter_lock_p.py
@@ -0,0 +1,25 @@
+from multiprocessing import Process
+import time
+
+def brew_chai():
+    print("Chai started brewing...")
+    count=0
+    for _ in range(10000000):
+        count+=1
+    print("Chai finished brewing...")
+
+p1=Process(target=brew_chai)
+p2=Process(target=brew_chai)
+
+start=time.time()
+
+p1.start()
+p2.start()
+
+p1.join()
+p2.join()
+
+end=time.time()
+
+print(f"Finished brewing in {end-start:.2f} seconds.")
+
