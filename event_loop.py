00df141606feb9cab7365606468ae848ec670b29
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-01-15 22:37:10 +0500

    FIFO operations in event loop

--- event_loop.py
+++ event_loop.py
@@ -8,9 +8,10 @@ async def fetch_data(t):
 
 
 async def main():
+    # create_task() makes tasks in the event loop with sequence
     task1=asyncio.create_task(fetch_data(1))
     task2=asyncio.create_task(fetch_data(2))
-
+    # In event loop,tasks are served as fifo
     resultt2= await task2
     print("Task 2 fully completed.")
     resultt1=await task1
