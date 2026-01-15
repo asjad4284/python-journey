4daa859db57505a48b35297c9b102c017f08a443
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-01-15 22:32:45 +0500

    Understand asyncio by rearranging the task to look how event loop works bts..

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello World")
+import asyncio
+
+async def fetch_data(t):
+    print(f"Do something with {t}...")
+    await asyncio.sleep(t)
+    print(f"Done with {t}")
+    return f"Result of {t}"
+
+
+async def main():
+    task1=asyncio.create_task(fetch_data(1))
+    task2=asyncio.create_task(fetch_data(2))
+
+    resultt2= await task2
+    print("Task 2 fully completed.")
+    resultt1=await task1
+    print("Task 1 fully completed.")
+    return [resultt1,resultt2]
+
+result=asyncio.run(main())
+print(result)
