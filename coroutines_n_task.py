6d352b05b63f37a8d318d1e06d5030917d7e2c23
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-01-18 22:25:13 +0500

    Working with coroutines

--- coroutines_n_task.py
+++ coroutines_n_task.py
@@ -13,6 +13,15 @@ async def main():
     result2=await t2
     print(f"Result of task 1 and 2 are: {[result1,result2]}")
 
+    # Gather Coroutines
+    coroutines=[f_data(i) for i in range(1,3)]
+    results=await asyncio.gather(*coroutines,return_exceptions=True)
+    print(f"Coroutine Results: {results}")
 
+
+
+start=time.time()
 result=asyncio.run(main())
+end=time.time()
+print(f"{end-start:.2f} seconds")
 # print(result)
