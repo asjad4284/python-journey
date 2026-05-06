736381ed20973364925a84041fd8d72b13562d6c
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-05-06 23:46:28 +0500

    Generators with examples

--- generators.py
+++ generators.py
@@ -30,3 +30,75 @@ def fibonacci(n):
         a, b = b, a + b
 
 print(list(fibonacci(10)))  # [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+# Generator that reads lines from a file
+def read_lines(filename):
+    with open(filename, 'r') as f:
+        for line in f:
+            yield line.strip()
+
+# Generator that filters even numbers
+def even_numbers(iterable):
+    for num in iterable:
+        if num % 2 == 0:
+            yield num
+
+# Generator that multiplies each value
+def multiply_by(iterable, factor):
+    for val in iterable:
+        yield val * factor
+
+# Chain generators together
+numbers = range(1, 11)
+evens = even_numbers(numbers)
+doubled = multiply_by(evens, 2)
+print(list(doubled))  # [4, 8, 12, 16, 20]
+
+# Generator with two-way communication (send)
+def averager():
+    total = 0
+    count = 0
+    while True:
+        value = yield total / count if count else 0
+        if value is None:
+            break
+        total += value
+        count += 1
+
+avg = averager()
+next(avg)  # Prime the generator
+print(avg.send(10))     # 10
+print(avg.send(20))     # 15
+print(avg.send(30))     # 20
+
+# Generator with close() to stop early
+def countdown(n):
+    while n > 0:
+        try:
+            yield n
+        except GeneratorExit:
+            print("Countdown cancelled!")
+            raise
+        n -= 1
+
+gen = countdown(5)
+print(next(gen))  # 5
+print(next(gen))  # 4
+gen.close()  # Stops the generator
+
+# Infinite generator for repeating values
+def repeat(obj):
+    while True:
+        yield obj
+
+counter = repeat("x")
+print([next(counter) for _ in range(3)])  # ['x', 'x', 'x']
+
+# Generator combining multiple iterables
+def chain(*iterables):
+    for iterable in iterables:
+        for item in iterable:
+            yield item
+
+combined = chain([1, 2], [3, 4], [5, 6])
+print(list(combined))  # [1, 2, 3, 4, 5, 6]
