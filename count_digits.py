db7ea9cddc68c4b8d17495c82505631df78618e5
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-07-10 21:53:52 +0500

    Logarithmic approach for count digits

--- count_digits.py
+++ count_digits.py
@@ -1,4 +1,9 @@
+from math import *
+
+
+
 num=123001
+
 n=0;
 while num>0:
     digit=num%10;
@@ -6,3 +11,11 @@ while num>0:
     n=n+1;
 
 print(n)
+
+#Logarithmic Approach
+
+def count_digits(number):
+    return int(log10(number)+1)
+
+print(count_digits(1234))
+
