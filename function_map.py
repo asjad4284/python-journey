4ff8f88c8292dbf9fedeab1f8e6da865f99946dd
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-07-17 23:29:37 +0500

    Getting started with hashmaps

--- function_map.py
+++ function_map.py
@@ -14,11 +14,22 @@ print(f_map)
 
 #method 2
 
+# number=[1,3,4,6,1,4,2,7,9,6,3,8,9]
+# n=len(number)
+# h_map=dict()
+
+# for i in range(0,n):
+#     h_map[number[i]]=h_map.get(number[i],0)+1
+
+# print(h_map)
+
 number=[1,3,4,6,1,4,2,7,9,6,3,8,9]
-n=len(number)
-h_map=dict()
+check=dict()
 
-for i in range(0,n):
-    h_map[number[i]]=h_map.get(number[i],0)+1
+for i in range(0,len(number)):
+    if number[i] in check:
+        check[number[i]]+=1
+    else:
+        check[number[i]]=1
 
-print(h_map)
+print(check)
