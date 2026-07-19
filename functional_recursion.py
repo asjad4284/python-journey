c17633964bbcafd4923043679d272c164a298ab1
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-07-19 21:07:44 +0500

    Factorial problem with recursion

--- functional_recursion.py
+++ functional_recursion.py
@@ -4,3 +4,12 @@ def func(n):
     return n+func(n-1)
 
 print(func(4))
+
+# Factorial with recursion
+
+def fact(n):
+    if n==1 or n==0:
+        return 1
+    return n*fact(n-1)
+
+print(fact(5))
