0c97a7234f453fe1139a5512498e900d4c66bb11
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-08-05 23:49:39 +0500

    Selection sort

---
+++ selection_sort.py
@@ -0,0 +1,13 @@
+def selection_sort(nums):
+    n=len(nums)
+    for i in range(0,n):
+        min_index=i
+        for j in range(i+1,n):
+            if nums[j]<nums[min_index]:
+                nums[min_index]=nums[j]
+        nums[i],nums[min_index]=nums[min_index],nums[i]
+
+
+lst=[5,4,1,7,8,2,3]
+selection_sort()
+print(lst)
