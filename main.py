14c81a119e9e33d2c5fb0508aa1af0872073c343
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-08-17 22:57:27 +0500

    Sorting algorithms

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-with open('sample.txt','a') as file:
-    file.write("This is second line line\n")
-    
+lst=[4,6,8,1,2,3]
+n=len(lst)
+
+for i in range(0,n):
+    min_index=i
+    for j in range(i+1,n):
+        if lst[min_index]>lst[j]:
+            min_index=j
+
+    lst[min_index],lst[i]=lst[i],lst[min_index]
+
+
+print(lst)
